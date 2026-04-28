ca874000b1e30d27c457bbcc4061ccf25bc46400
prajwal24 <prajwaldevsinghrajput@gmail.com>
2026-04-29 04:04:58 +0530

    Initial Grocify setup

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center  ">
+      
+      <Text className="text-red-500 text-2xl font-bold">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
